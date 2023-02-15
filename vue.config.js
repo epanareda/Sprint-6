931ec019cb801ff2357a3234d76e664904db52e3
epanareda <epanareda@gmail.com>
2023-02-15 21:59:47 +0100

    vue.config.js changed so once build it can be opened locally

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // Yhe following line is to avoid the error when vue files are one word (Home or Escena)
-  lintOnSave: false
+  // The following line is to avoid the error when vue files are one word (Home or Escena)
+  lintOnSave: false,
+  // The following line allows to open the project locally via the index.html without using any server (creating the project with "npm run build")
+  publicPath: "",
 })
